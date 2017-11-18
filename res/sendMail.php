a64f983e127b9e1ecb5809609f0317cb15f0f80e
Macbook <nalinsuri@gmail.com>
2017-11-18 14:52:59 -0500

    Check to see if user has at least entered something in the name field before sending email.

--- res/sendMail.php
+++ res/sendMail.php
@@ -11,15 +11,19 @@
     $phone = $_POST['telephone'];
     $message = $_POST['comments'];
 
-    $finalMessage = "Sender: " . $firstName . " " . $lastName;
-    $finalMessage .= "\nEmail: " . $email;
-    $finalMessage .= "\nTelephone: " . $phone;
-    $finalMessage .= "\nMessage Content: " . $message;
+    if (strlen($firstName . $lastName) < 1) {
+      echo "No name provided. Mail will be discarded.";
+    } else {
+      $finalMessage = "Sender: " . $firstName . " " . $lastName;
+      $finalMessage .= "\nEmail: " . $email;
+      $finalMessage .= "\nTelephone: " . $phone;
+      $finalMessage .= "\nMessage Content: " . $message;
 
-    mail('suriosity@gmail.com', "Contact Message From Website", $finalMessage);
-
-  exit();
+      mail('suriosity@gmail.com', "Contact Message From Website", $finalMessage);
+      echo "Thank you for contacting me, I will get back to you shortly!";
+    }
 ?>
-<h1> Thank You for contacting me, I will get back to you shortly!</h1>
-<h2> You will be redirected back to the home-page in 3 seconds!!</h2>
+
+<div>You will be redirected back to the home-page in 3 seconds!!</div>
+
 </body>
